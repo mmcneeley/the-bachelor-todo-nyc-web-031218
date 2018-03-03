b9a1c082b32d76bc399869d14dac9cfc4b0b80eb
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 23:55:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,7 +21,17 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+
+  data.each do |season, season_value|
+    season_value.each do |attribute|
+      if attribute["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+
+  counter
 end
 
 def get_occupation(data, hometown)
