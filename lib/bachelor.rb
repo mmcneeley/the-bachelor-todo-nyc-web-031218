789168cda14a9d0c83a4f6f67089b70f159a584f
Mike McNeeley <mmcneeley@gmail.com>
2018-03-03 23:49:06 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,23 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |people|
+      binding.pry
+      if people["status"].downcase == "winner"
+        return people["name"].split(" ").first
+      end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, season_value|
+    season_value.each do |attribute|
+      binding.pry
+      if attribute["occupation"] == occupation
+        return attribute["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
